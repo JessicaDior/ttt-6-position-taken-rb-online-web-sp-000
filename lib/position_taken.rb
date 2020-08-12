58da076e6c70a641b4920184d68d58804876454c
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-12 00:51:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
-# code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+index =
+
+def position_taken?(board, index)
+
+if board[index] != " " or "" or nil
+  true
+elsif
+   board[index] = " " or "" or nil
+false
+
+end
+
+end
